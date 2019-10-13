2777e8d27b96aec4cdfac693bfa0b9dcd571c401
kiranykule <kiran.waykule9139@gmail.com>
2019-10-13 15:00:24 +0530

    dsfas

---
+++ advance.php
@@ -0,0 +1,71 @@
+	<?php include('header.php'); ?>
+				<!-- END: .app-side -->
+
+				<!-- BEGIN .app-main -->
+				<div class="app-main">
+					<!-- BEGIN .main-heading -->
+				
+					<!-- END: .main-heading -->
+					<!-- BEGIN .main-content -->
+					<div class="main-content">
+						<style type="text/css">
+							h3{text-align: center; font-weight: 600; color:#5d36b1;}
+						</style>
+						<!-- Row start -->
+						<div class="row gutters" >
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+								<div class="card">
+									
+									<div class="card-body" style="overflow: auto;">
+										<h3>Advance List</h3>
+										<div class="table-responsive">
+										<table id="basicExample" class="table table-striped table-bordered">
+											<thead>
+												<tr>
+													<th>Sr.No</th>
+													<th>Customer Name</th>
+													<th>Mobile</th>
+													<th>Date</th>
+													<th>	Previous Amount</th>
+													<th>	Payable Amount</th>
+													<th>	Total Amount</th>
+
+
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Tiger Nixon</td>
+													<td>System Architect</td>
+													<td>61545522</td>
+													<td>2011/04/25</td>
+													<td>System Architect</td>
+													<td>61545522</td>
+													<td>2011/04/25</td>
+													
+													
+													
+												</tr>
+												
+											</tbody>
+										</table>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Row ends -->
+
+
+					
+						
+					</div>
+					<!-- END: .main-content -->
+				</div>
+				<!-- END: .app-main -->
+			</div>
+			<!-- END: .app-container -->
+			<!-- BEGIN .main-footer -->
+			<
+		
+	<?php include('footer.php'); ?>
